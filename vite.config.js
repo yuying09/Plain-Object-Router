b821558bd5f3c6cacb17a7e5665cc73cadd34338
yuying <yuying9219@gmail.com>
2025-03-04 17:14:13 +0800

    更換repo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV==='production'? '/Plain-Object-Router/':'/',
+  //這裡有更改網址 從React-router換成Plain-Object-Router
   plugins: [react()],
 })
